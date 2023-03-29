b1226174ffcefa940261673d7b9a2b57ed09e617
ando <terbu102@gmail.com>
2023-03-29 03:08:22 -0700

    fun 2

--- functions2.c
+++ functions2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/****************** PRINT POINTER ******************/
+/***************** PRINT POINTER ******************/
 /**
  * print_pointer - Prints the value of a pointer variable
  * @types: List a of arguments
